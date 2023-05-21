f8fd22f509abbce01ca46de93213d6bf9140667f
NimaSabzeh <nsab7998@uni.sydney.edu.au>
2023-05-21 19:13:06 +1000

    Menu

--- public/script.js
+++ public/script.js
@@ -6,12 +6,21 @@ window.onload = function (){
 }
 
 function handleRoute(page){
-    console.log(page)
     window.location = `index.html?page=${page}`
-
 }
 
+function menuOverlay (x){
+    if (x.classList.contains("changex")){
+        console.log("true")
+        document.getElementById("menu").style.opacity = 1;
+        document.getElementById("menu").style.zIndex = 99;
+    } else {
+        console.log("false")
+        document.getElementById("menu").style.opacity = 0;
+        document.getElementById("menu").style.zIndex = -1;
+    }
 
+}
 
 
 function menuIcon(x) {
